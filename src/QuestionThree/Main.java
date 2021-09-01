f486d85c43d06f9776af3e4d2275014890c6862d
Leonardo-Messias <leonardo.messias@hotmail.com>
2021-09-01 14:52:56 -0300

    att

--- src/QuestionThree/Main.java
+++ src/QuestionThree/Main.java
@@ -15,12 +15,3 @@ public class Main {
 }
 
 
-//            Scanner imp = new Scanner(System.in);
-//            System.out.println("Digite o seu nome: ");
-//            morador.setNome(imp.nextLine());
-//            System.out.println("Digite o numero da residencia: ");
-//            morador.setNumero(imp.nextInt());
-//            System.out.println("Digite seu numero de telefone: ");
-//            morador.setTelefone(imp.nextInt());
-//            System.out.println("Possui cachorro na residencia?\n 1=sim ou 2=nao");
-//            morador.setDog(imp.nextInt());
