f781c08851a10aeb68ebcaebd868f10b46f5fb15
Leonardo-Messias <leonardo.messias@hotmail.com>
2021-09-01 14:31:57 -0300

    att

--- src/QuestionThree/Main.java
+++ src/QuestionThree/Main.java
@@ -1,32 +1,23 @@
 package QuestionThree;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Morador morador = new Morador();
-        int cont=0;
-        Scanner imp = new Scanner(System.in);
-        //for (int i=0; i<5; i++) {
-            System.out.println("Digite o seu nome: ");
-            morador.nome = imp.nextLine();
-            System.out.println("Digite o numero da residencia: ");
-            morador.numerores = imp.nextInt();
-            System.out.println("Digite seu numero de telefone: ");
-            morador.telefone = imp.nextInt();
-            System.out.println("Possui cachorro na residencia?\n 1=sim ou 2=nao");
-            morador.dog = imp.nextInt();
-        //}
-       if(morador.dog ==1){
-            System.out.println("!!Entregamos a correspondencia!!");
-        }else{
-            System.out.println("!!Nao entregamos a correspondencia");
-          }
-//        if(morador.numerores % 2 != 0){
-//            cont++;
-//            System.out.println("Quantidade de casas impares: "+cont);
-//
-//        }
+        Morador morador = new Morador("Leonardo Messias",true,997446522,302);
+        morador.status();
+        morador.entrega();
+
+
     }
 
 }
+
+
+//            Scanner imp = new Scanner(System.in);
+//            System.out.println("Digite o seu nome: ");
+//            morador.setNome(imp.nextLine());
+//            System.out.println("Digite o numero da residencia: ");
+//            morador.setNumero(imp.nextInt());
+//            System.out.println("Digite seu numero de telefone: ");
+//            morador.setTelefone(imp.nextInt());
+//            System.out.println("Possui cachorro na residencia?\n 1=sim ou 2=nao");
+//            morador.setDog(imp.nextInt());
--- src/QuestionThree/Morador.java
+++ src/QuestionThree/Morador.java
@@ -1,8 +1,60 @@
 package QuestionThree;
 
 public class Morador {
-    String nome;
-    Integer dog;
-    Integer telefone;
-     Integer numerores;
+    public String nome;
+    private Integer telefone;
+    private Integer numero;
+    public Boolean dog;
+
+    public Morador(String nome, Boolean dog, Integer telefone, Integer numero) {
+        this.setNome(nome);
+        this.setDog(dog);
+        this.setTelefone(telefone);
+        this.setNumero(numero);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Boolean getDog() {
+        return dog;
+    }
+
+    public void setDog(Boolean dog) {
+        this.dog = dog;
+    }
+
+    public Integer getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Integer telefone) {
+        this.telefone = telefone;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    void status(){
+        System.out.println("Seu nome: "+this.getNome());
+        System.out.println("Numero da residencia: "+this.getNumero());
+        System.out.println("Telefone: "+this.getTelefone());
+    }
+    void entrega(){
+        if (this.dog==false){
+            System.out.println("Entregamos em sua residencia");
+        }else{
+            System.out.println("Nao entregamos em sua residencia");
+        }
+    }
 }
+
